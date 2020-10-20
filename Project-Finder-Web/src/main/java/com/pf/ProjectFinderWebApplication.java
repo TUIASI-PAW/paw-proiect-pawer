d4bedb1124e39bdc094483c42d9a847f1f705a2f
BACIU ALEXANDRU <alexandruuubaciu@gmail.com>
2020-10-20 18:00:53 +0300

    create structure for backend application

---
+++ Project-Finder-Web/src/main/java/com/pf/ProjectFinderWebApplication.java
@@ -0,0 +1,13 @@
+package com.pf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectFinderWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectFinderWebApplication.class, args);
+    }
+
+}
