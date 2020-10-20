8e2de21b39d041273f0020379e0d42c8b20f05dd
BACIU ALEXANDRU <alexandruuubaciu@gmail.com>
2020-10-20 18:20:12 +0300

    create structure for client application

---
+++ Project-Finder-Web/src/test/java/com/pf/ProjectFinderWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectFinderWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
